b0f49f1b776909b65023c825f441d31afbe2e090
Mika Higashizima <144198758+higashizima@users.noreply.github.com>
2023-10-22 15:40:24 +0000

    Initial Commit4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,25 +1,25 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:top, :about]
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
   def after_sign_in_path_for(resource)
-    "/users/#{current_user.id}"
+    user_path(current_user.id)
   end
-  
-  def after_sign_out_path_for(resource) 
-    "/"
+
+  def after_sign_out_path_for(resource)
+    root_path
   end
-  
+
 
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
     devise_parameter_sanitizer.permit(:account_update, keys: [:introduction])
   end
-  
-  
+
+
   def application_create
      @book = Book.new(book_params)
      @book.user_id = current_user.id
@@ -27,10 +27,10 @@ class ApplicationController < ActionController::Base
       flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book.id)
     else
-      @books = Book.all 
+      @books = Book.all
       render :index
     end
   end
-  
-  
+
+
 end
